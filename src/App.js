776f9339f4d54f1be1aec173f3e81321003dbeb5
michellebakels <michelle.bakels@gmail.com>
2021-07-26 11:54:12 -0400

    add state and implement button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from "./Components/Header"
 import Footer from "./Components/Footer";
+import RepresentativeSearch from "./Components/RepresentativeSearch";
 
 function App() {
   return (
     <div>
       <Header />
+      <RepresentativeSearch/>
       <Footer />
     </div>
   );
